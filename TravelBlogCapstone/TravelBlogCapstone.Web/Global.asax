bc0695066368e574d2c33023318bac38cafde39a
Andrew Rowe <arowe77@gmail.com>
2016-08-10 14:17:50 -0400

    Edit

--- TravelBlogCapstone/TravelBlogCapstone.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TravelBlogCapstone.Web.MvcApplication" Language="C#" %>
--- TravelBlogCapstone/TravelBlogCapstone.Web/Scripts/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="jquery-1.9.1.js" />
-/// <reference path="jquery-ui-1.10.0.js" />
-/// <reference path="jquery-1.11.1.intellisense.js" />
-/// <reference path="bootstrap.js" />
